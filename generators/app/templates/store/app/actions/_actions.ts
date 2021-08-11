79addba89f7e9f2430a418d9fc7dd4e6a8690b3d
Josh Walters <josh.walters@outlook.com>
2021-08-11 16:52:12 -0500

    updated actions import order

--- generators/app/templates/store/app/actions/_actions.ts
+++ generators/app/templates/store/app/actions/_actions.ts
@@ -1,5 +1,5 @@
-import setAppBusy from './set-app-busy';
 import { actionTypes } from './_types';
+import setAppBusy from './set-app-busy';
 
 export const actions = {
   [actionTypes.setAppBusy]: setAppBusy,
