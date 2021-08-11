1d7d6f66d2daba52dcd8720fbf62337177c5cbd9
Josh Walters <josh.walters@outlook.com>
2021-08-11 16:49:29 -0500

    updated ActionContext interface updated version number

--- generators/app/templates/store/action-context.ts
+++ generators/app/templates/store/action-context.ts
@@ -4,6 +4,7 @@ export interface ActionContext<State = any> {
   state: State;
   rootState: RootState;
   getters: { [key: string]: any };
-  dispatch: <T = any>(action: string, payload?: T) => Promise<any>;
-  commit: <T = any>(mutation: string, payload?: T) => void;
+  rootGetters: { [key: string]: any };
+  commit: <Payload = any>(mutationType: string, payload?: Payload) => void;
+  dispatch: <Payload = any, Result = any>(actionType: string, payload?: Payload) => Promise<Result>;
 }
